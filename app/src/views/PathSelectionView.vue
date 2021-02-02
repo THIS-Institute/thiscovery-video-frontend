3333d82b59cb830e3954914e2b85fc9e6abd0f45
Matt Slater <matthewslater1998@gmail.com>
2021-02-02 12:57:49 +0000

    Adds some basic global styles

---
+++ app/src/views/PathSelectionView.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="e-container">
+		<component
+			:is="`h${index + 1}`"
+			v-for="(title, index) in 4"
+			:key="index"
+			:class="`e-h${index + 1}`"
+			v-text="message"
+		/>
+	</div>
+</template>
+
+<script>
+	import { reactive, toRefs } from 'vue';
+
+	export default {
+		setup () {
+			const state = reactive({
+				message: "This is a new way of doing research",
+			});
+		
+			return {
+				...toRefs(state),
+			};
+		},
+	};
+</script>
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,15 +1,92 @@
+const mapKeys = require('lodash/mapKeys');
+const mapValues = require('lodash/mapValues');
+const range = require('lodash/range');
+
+const variables = require('./variables.json');
+
+// converters and calculators
+const relative = (px, unit = 'rem', base = variables['browser-default-font-size']) => `${px / base}${unit}`;
+const ratio = (x, y) => `${y / x * 100}%`;
+
+// Values
+const screens = mapValues(variables.breakpoints, px => relative(px, 'em'));
+const colors = variables['colors'];
+const columns = variables.columns;
+const widths = mapKeys(mapValues(range(0, columns), (v) => ratio(columns, v + 1)), (v, k) => `${parseInt(k, 10) + 1}/${columns}`);
+const z = variables['z-indexes'];
+const zIndex = z.reduce((v, name, i) => ({ ...v, [name]: z.length - i }), {});
+
 module.exports = {
-    purge: [
-        './public/index.html',
-        './src/**/*.js',
-        './src/**/*.vue',
-    ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+	theme: {
+		screens,
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			inherit: 'inherit',
+			...colors,
+		},
+		fontSize: {
+			sm: relative(14),
+			base: relative(16),
+			lg: relative(18),
+			xl: relative(20),
+			'2xl': relative(24),
+			'3xl': relative(32),
+			'4xl': relative(38),
+			'5xl': relative(58),
+			full: '100%',
+		},
+		fontWeight: {
+			normal: 400,
+			bold: 700,
+		},
+		letterSpacing: {
+			tight: '-1px',
+			snug: '-0.66px',
+			normal: 0,
+		},
+		lineHeight: {
+			none: 1,
+			tight: 1.12,
+			snug: 1.4,
+			normal: 1.56,
+			relaxed: 1.75,
+			loose: 2,
+		},
+		zIndex: {
+			...zIndex,
+			0: 0,
+			'-1': -1,
+		},
+		extend: {
+			inset: (theme, { negative }) => ({
+				'1/2': '50%',
+				...widths,
+				...(negative(widths)),
+			}),
+			spacing: {
+				em: '1em',
+				'1/2em': '.5em',
+
+				gutter: relative(20),
+				margin: relative(60),
+			},
+			maxWidth: {
+				container: relative(1440),
+			},
+			width: {
+				...widths,
+			},
+		},
+	},
+	variants: {},
+	corePlugins: {
+		container: false,
+	},
+	purge: [
+		'./public/index.html',
+		'./src/**/*.js',
+		'./src/**/*.vue',
+	],
+	darkMode: false,
 }
