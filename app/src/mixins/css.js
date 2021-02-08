5c334520a9ab41054118b6d6dc2f3a5e7eff94ea
Matthew Wilkinson <matthew@engageinteractive.co.uk>
2021-02-08 20:25:32 +0000

    Removes unused mixins

--- app/src/mixins/css.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-	sort: breakpoint => ['width', 'height']
-		.filter(dimension => breakpoint[dimension])
-		.map(dimension => `(min-${dimension}: ${relative(breakpoint[dimension], 'em')})`)
-		.join(' and '),
-	relative,
-};
