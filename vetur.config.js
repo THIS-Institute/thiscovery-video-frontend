7042f9a1780a963c5aff28fc8b231cb5d19253cc
Matthew Wilkinson <matthew@engageinteractive.co.uk>
2021-02-15 10:13:56 +0000

    Adds vetur config to point to frontend project root

---
+++ vetur.config.js
@@ -0,0 +1,11 @@
+// Tells Vetur that the Vue project is in 'app'.
+
+module.exports = {
+    settings: {
+        "vetur.useWorkspaceDependencies": true,
+        "vetur.experimental.templateInterpolationService": true
+    },
+    projects: [
+        './app',
+    ],
+}
