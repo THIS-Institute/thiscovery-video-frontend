516e1bf89a827a8a905864f72ce848495c63a035
Matt Slater <matthew.slater@engageinteractive.co.uk>
2021-02-17 10:17:34 +0000

    Adds grid to self record view

--- app/src/views/SelfRecord.vue
+++ app/src/views/SelfRecord.vue
@@ -1,3 +1,15 @@
 <template>
-	<pre>Self record</pre>
+	<section class="e-container h-full">
+		<div class="grid items-center grid-cols-12 gap-5">
+			<div
+				:class="[
+					'col-span-12',
+					'sm:col-span-10 sm:col-start-2',
+					'md:col-span-8 md:col-start-3',
+				]"
+			>
+				<pre>Self record</pre>
+			</div>
+		</div>
+	</section>
 </template>
