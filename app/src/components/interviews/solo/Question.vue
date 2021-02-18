d9c2efaf17ebf729229896f6ac18c031fdb653f2
Matt Slater <matthew.slater@engageinteractive.co.uk>
2021-02-18 14:28:48 +0000

    Adds markup to Solo Question component

---
+++ app/src/components/interviews/solo/Question.vue
@@ -0,0 +1,38 @@
+<template>
+	<icon-text
+		icon="question"
+		:text="`Question ${number}`"
+		class="e-h4"
+	/>
+
+	<h3
+		class="e-h3 mt-7.5"
+		v-text="title"
+	/>
+
+	<p
+		class="text-sm mt-2.5"
+		v-text="description"
+	/>
+</template>
+
+<script>
+	export default {
+		props: {
+			number: {
+				type: Number,
+				default: null,
+			},
+
+			title: {
+				type: String,
+				default: null,
+			},
+
+			description: {
+				type: String,
+				default: null,
+			},
+		},
+	};
+</script>
