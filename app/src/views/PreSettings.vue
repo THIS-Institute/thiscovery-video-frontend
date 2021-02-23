5ff0308a9c162d191e0eec756c5906bed215f251
Matt Slater <matthew.slater@engageinteractive.co.uk>
2021-02-23 15:09:56 +0000

    Adds video Wrapper to pre settings view

--- app/src/views/PreSettings.vue
+++ app/src/views/PreSettings.vue
@@ -4,12 +4,7 @@
 			<div class="col-span-12 sm:col-span-8 sm:col-start-3 lg:col-span-4 lg:col-start-5">
 				<div class="bg-white rounded-lg w-full">
 					<div class="p-2.5 pb-16 w-full">
-						<placeholder
-							ratio="pt-3/4"
-							class="rounded-lg overflow-hidden"
-						>
-							<video-preview />
-						</placeholder>
+						<video-wrapper class="rounded-lg" />
 
 						<div class="flex flex-col items-center mt-12">
 							<h2 class="e-h3 text-center">
@@ -36,11 +31,11 @@
 </template>
 
 <script>
-	import VideoPreview from '@/components/interviews/settings/VideoPreview';
+	import VideoWrapper from '@/components/interviews/settings/VideoWrapper';
 
 	export default {
 		components: {
-			VideoPreview
+			VideoWrapper
 		},
 	};
 </script>
