ac6e963d210974b262ae3618bbd768d626914fa0
Matthew Wilkinson <matthew@engageinteractive.co.uk>
2021-02-24 11:20:03 +0000

    Adds Twilio room token endpoint

--- src/room_token.py
+++ src/room_token.py
@@ -1,7 +1,53 @@
 import json
+import os
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant
 
-def lambda_handler(event, context):
+def get_access_token(identity, room):
+    token = AccessToken(
+        os.environ['TWILIO_ACCOUNT_SID'],
+        os.environ['TWILIO_API_KEY'],
+        os.environ['TWILIO_API_SECRET']
+    )
+    
+    token.identity = identity
+    
+    grant = VideoGrant()
+    grant.room = room
+    token.add_grant(grant)
+    
+    return token.to_jwt()
+
+def get_error_response(message):
+    response_body = {
+        'error': True,
+        'message': message
+    }
+
+    return {
+        'statusCode': 422,
+        'body': json.dumps(response_body)
+    }
+
+def get_token_response(token):
+    response = {
+        'access_token': str(token)
+    }
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(response)
     }
+
+def lambda_handler(event, context):
+    request_body = json.loads(event['body'])
+
+    if not 'identity' in request_body:
+        return get_error_response(message='Request must contain identity')
+
+    if not 'room' in request_body:
+        return get_error_response(message='Request must contain room UUID')
+
+    token = get_access_token(identity=request_body['identity'], room=request_body['room'])
+
+    return get_token_response(token=token)
