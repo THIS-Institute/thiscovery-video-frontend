2817f44ceb642e3fba6bab68ab1e60632a956bbd
Matt Slater <matthew.slater@engageinteractive.co.uk>
2021-03-04 16:27:50 +0000

    Moves pre settings hard coded copy to props from content file

--- app/src/views/PreSettings.vue
+++ app/src/views/PreSettings.vue
@@ -4,22 +4,29 @@
 			<div class="col-span-12 sm:col-span-8 sm:col-start-3 lg:col-span-4 lg:col-start-5">
 				<div class="bg-white rounded-lg w-full">
 					<div class="p-2.5 pb-16 w-full">
-						<video-wrapper class="rounded-lg overflow-hidden" />
+						<video-wrapper
+							class="rounded-lg overflow-hidden"
+							pre-record
+						/>
 
 						<div class="flex flex-col items-center mt-12">
-							<h2 class="e-h3 text-center">
-								All set to begin?
-							</h2>
+							<h2
+								class="e-h3 text-center"
+								v-text="title"
+							/>
 
-							<p class="text-sm text-center mt-1.5">
-								Access to your microphone is required. Video access is encouraged but you can disable your camera using the controls above if you wish.
-							</p>
+							<p
+								v-if="content"
+								class="text-sm text-center mt-1.5"
+								v-text="content"
+							/>
 
 							<e-button
-								title="See the first question"
+								v-if="cta"
+								:title="cta.title"
 								icon="chevron-right"
 								class="e-button--red mt-5"
-								:url="{ name: 'self_interview' }"
+								:url="cta.url"
 								pill
 							/>
 						</div>
@@ -36,6 +43,23 @@
 	export default {
 		components: {
 			VideoWrapper
+		},
+
+		props: {
+			title: {
+				type: String,
+				required: true,
+			},
+
+			content: {
+				type: String,
+				default: null,
+			},
+
+			cta: {
+				type: Object,
+				default: null,
+			},
 		},
 	};
 </script>
