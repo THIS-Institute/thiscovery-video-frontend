8f1e879bd650e9f4ca2da623b82739ae787b386b
Matthew Wilkinson <matthew@engageinteractive.co.uk>
2021-03-18 17:03:07 +0000

    Adds appointments gateway endpoints

--- src/log.py
+++ src/log.py
@@ -1,3 +1,12 @@
 def lambda_handler(event, context):
     print('## EVENT')
     print(event)
+
+    return {
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        },
+        'statusCode': 200,
+        'body': '\{\}'
+    }
