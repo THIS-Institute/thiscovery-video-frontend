5fbbb1563fc72436c915e5daebf1be4753dec284
Matthew Wilkinson <matthew@engageinteractive.co.uk>
2021-04-14 10:06:26 +0100

    Adds requests toolbelt to uplaod handler

--- src/upload_video_answer.py
+++ src/upload_video_answer.py
@@ -1,11 +1,15 @@
 import os
 import json
 import base64
+from requests_toolbelt.multipart import decoder
 
 def lambda_handler(event, context):
     form_data = base64.b64decode(event['body'])
 
-    print(form_data)
+    multipart_data = MultipartDecoder(form_data, 'multipart/form-data')
+
+    for part in multipart_data.parts:
+        print(part)
 
     return {
         'headers': {
