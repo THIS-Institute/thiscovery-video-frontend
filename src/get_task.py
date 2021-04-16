bff52cf5bd085974fb6926bd0f84b1b07f4e246f
Matthew Wilkinson <matthew@engageinteractive.co.uk>
2021-04-16 15:40:08 +0100

    Adds get task endpoint

---
+++ src/get_task.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+def lambda_handler(event, context):
+    return {
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        },
+        'statusCode': 200,
+        'body': json.dumps({
+            'id': '0fda6eff-b1e5-44df-93b4-3d71c03adeff',
+            'acuityTypeId': '20973054',
+            'onDemandAvailable': True,
+            'liveAvailable': True,
+            'title': 'Suggest recommendation for good practice',
+        }),
+    }
