133d4d2395b2bc7d6de3ed9bbb2771fd74cbd649
Matthew Wilkinson <matthew@engageinteractive.co.uk>
2021-06-08 22:55:04 +0100

    Adds composition handler

--- src/live_recording.py
+++ src/live_recording.py
@@ -1,7 +1,44 @@
+import os
 import json
+import boto3
+from urllib.request import urlopen
+from secrets import SecretsManager
+from twilio.rest import Client
 
 def lambda_handler(event, context):
-    print(event)
+    detail = event['detail']
+
+    secret = SecretsManager(os.environ['SECRETS_NAMESPACE'])
+
+    client = Client(
+        secret.get('twilio-api-key'),
+        secret.get('twilio-api-secret')
+    )
+
+    composition_url = detail['MediaUri']
+    room_sid = detail['RoomSid']
+
+    uri = f'https://video.twilio.com{composition_url}'
+    response = client.request('GET', uri)
+
+    media_location = json.loads(response.text).get('redirect_to')
+
+    temp_file = f'/tmp/{room_sid}.mp4'
+
+    with open (temp_file, 'wb') as file:
+        file.write(urlopen(media_location).read())
+
+    s3 = boto3.client('s3')
+
+    s3.upload_file(
+        Filename=temp_file,
+        Bucket=os.environ['BUCKET_NAME'],
+        Key=f'live/{room_sid}.mp4',
+    )
+
+    os.remove(temp_file)
+
+    client.video.compositions(detail['CompositionSid']).delete()
 
     return {
         'statusCode': 200,
