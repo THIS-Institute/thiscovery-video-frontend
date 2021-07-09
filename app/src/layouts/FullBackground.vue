4b5a3af627d37c1b269b1a1dce2eb39f4a0fc62c
Matt Slater <matthew.slater@engageinteractive.co.uk>
2021-07-09 16:01:58 +0100

    Removes fullback modal testing example

--- app/src/layouts/FullBackground.vue
+++ app/src/layouts/FullBackground.vue
@@ -32,38 +32,6 @@
 			/>
 
 			<router-view />
-
-			<div class="e-container">
-				<div class="flex space-x-4">
-					<x-button
-						title="Leave interview"
-						class="e-button--red"
-						type="pill"
-						@click="openModal(modals.leave, leaveCallbacks)"
-					/>
-
-					<x-button
-						title="Retake"
-						class="e-button--red-outline"
-						type="pill"
-						@click="openModal(modals.retake)"
-					/>
-
-					<x-button
-						title="Join by phone"
-						class="e-button--red"
-						type="pill"
-						@click="openModal(modals.phone)"
-					/>
-
-					<x-button
-						title="How to fix this"
-						class="e-button--red-outline"
-						type="pill"
-						@click="openModal(modals.troubleshoot)"
-					/>
-				</div>
-			</div>
 		</main>
 	</div>
 </template>
@@ -71,7 +39,6 @@
 <script>
 	import { computed } from 'vue';
 	import { useStore } from 'vuex';
-	import modals from '@/modals';
 
 	import AppHeader from '@/domain/app/AppHeader';
 	import AppNavigation from '@/domain/app/AppNavigation';
@@ -105,17 +72,7 @@
 			const navActive = computed(() => store.state.app.navActive);
 			const nav = computed(() => store.state.app.nav);
 
-			const openModal = (options, callbacks) => store.dispatch('app/openModal', { ...options, callbacks });
-
-			const onLeave = () => console.log('leave');
-			const leaveCallbacks = {
-				confirm: () => onLeave(),
-			};
-
 			return {
-				modals,
-				leaveCallbacks,
-				openModal,
 				navActive,
 				nav,
 			};
